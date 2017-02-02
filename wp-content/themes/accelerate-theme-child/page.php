23ad0e30b9fd6172893b9e44cc6c1c972a4722dd
Bee <beemrtinez@gmail.com>
2017-02-02 13:54:11 -0600

    back up one

--- wp-content/themes/accelerate-theme-child/page.php
+++ wp-content/themes/accelerate-theme-child/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the homepage
+ * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,15 +14,14 @@
 
 get_header(); ?>
 
-<section class="home-page">
-	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class='homepage-hero'>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
-			</div>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .home-page -->
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
